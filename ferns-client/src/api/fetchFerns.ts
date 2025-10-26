2f22686090da0fc1bb03e93e922606607793fdfc
Jack <primes_unit.05@icloud.com>
2025-10-27 11:59:34 +1300

    add prod endpoint

--- ferns-client/src/api/fetchFerns.ts
+++ ferns-client/src/api/fetchFerns.ts
@@ -1,5 +1,5 @@
 export async function fetchFerns() {
-  const response = await fetch("/api/ferns");
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/ferns`);
   if (!response.ok) throw new Error("Failed to fetch ferns");
   return response.json();
 }
